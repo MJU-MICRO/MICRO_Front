989eeba9fb30321324257a9c6f07e3fbbdfef608
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2023-07-18 16:32:33 +0900

    [Chore] 불필요한 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SiteIntroduction from "./screen/SiteIntroduction";
-import ClubMajorAcademyList from "./screen/ClubMajorAcademyList";
-import StudentCouncilList from "./screen/StudentCouncilList";
-import CommunityList from "./screen/SmallGroupList";
-import CompetitionList from "./screen/CompetitionList";
-import Login from "./screen/Login";
-import SignUp from "./screen/SignUp";
-import Profile from "./screen/Profile";
-import Home from "./screen/Home";
-import ReceiveMessage from "./screen/ReceiveMessage";
-import Header from "./component/Header";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SiteIntroduction from './screen/SiteIntroduction';
+import ClubMajorAcademyList from './screen/ClubMajorAcademyList';
+import StudentCouncilList from './screen/StudentCouncilList';
+import CommunityList from './screen/SmallGroupList';
+import CompetitionList from './screen/CompetitionList';
+import Login from './screen/Login';
+import SignUp from './screen/SignUp';
+import Profile from './screen/Profile';
+import Home from './screen/Home';
+import ReceiveMessage from './screen/ReceiveMessage';
+import Header from './component/Header';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App() {
@@ -18,16 +18,16 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/site-introduction" element={<SiteIntroduction />} />
-        <Route path="/club-majorAcademy" element={<ClubMajorAcademyList />} />
-        <Route path="/student-councils" element={<StudentCouncilList />} />
-        <Route path="/communities" element={<CommunityList />} />
-        <Route path="/competitions" element={<CompetitionList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/receive-message" element={<ReceiveMessage />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/site-introduction' element={<SiteIntroduction />} />
+        <Route path='/club-majorAcademy' element={<ClubMajorAcademyList />} />
+        <Route path='/student-councils' element={<StudentCouncilList />} />
+        <Route path='/communities' element={<CommunityList />} />
+        <Route path='/competitions' element={<CompetitionList />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/receive-message' element={<ReceiveMessage />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </Router>
   );
