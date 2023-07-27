c96c4c3b25d7b5a330353ae3b52853e0ba0fd0fd
lya0210 <lya8690@naver.com>
2023-07-27 23:02:28 +0900

    [feat] 단체 상세 조회 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import RecruitmentList from 'screen/Recruitment/RecruitmentList';
 import OrganizationList from './screen/Organization/OrgaizationList';
 import SignUp from './screen/SignUp';
+import ClubDetail from './screen/Organization/ClubDetail';
+import StudentCouncilDetail from './screen/Organization/StudentCouncilDetail';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/recruitmentList' element={<RecruitmentList />} />
         <Route path='/organizationList' element={<OrganizationList />} />
+        <Route path='/club/:id' element={<ClubDetail />} />
+        <Route path='/studentCouncil/:id' element={<StudentCouncilDetail />} />
         <Route path='/signUp' element={<SignUp />} />
       </Routes>
     </Router>
