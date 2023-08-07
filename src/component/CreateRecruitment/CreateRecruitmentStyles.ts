e3c86a8d609d7831117623b32e98312ec8d01fdb
JunnKyuu <junnkyuu22@gmail.com>
2023-08-07 18:30:30 +0900

    [FEAT]#14 모집 공고 등록 페이지 1 디자인

--- src/component/CreateRecruitment/CreateRecruitmentStyles.ts
+++ src/component/CreateRecruitment/CreateRecruitmentStyles.ts
@@ -8,19 +8,212 @@ export const RecruitmentContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  border-bottom: 0.5px solid #dddddd;
-  background-color: white;
+  margin-bottom: 0.7rem;
 `;
 
-export const MediumText = styled.span`
+export const InfoContainer = styled.div`
+  width: 26.625rem;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+export const GroupContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 1.5rem;
+  border-bottom: 0.5px solid #dddddd;
+`;
+
+export const BasicNoticeTitle = styled.span`
   color: #000;
   leading-trim: both;
   text-edge: cap;
   font-family: GmarketSansMedium;
-  font-size: 0.9375rem;
+  font-size: 1.1rem;
   font-style: normal;
   font-weight: 500;
   line-height: normal;
 `;
 
-export const LightText = styled.span``;
+export const BasicNoticeText = styled(BasicNoticeTitle)`
+  font-size: 0.9375rem;
+`;
+
+export const RedAsterisk = styled(BasicNoticeText)`
+  color: red;
+`;
+
+export const TextContainer = styled.div`
+  margin-top: 1.2rem;
+  margin-bottom: 0.7rem;
+`;
+
+export const GroupName = styled.span`
+  color: #000;
+  leading-trim: both;
+  text-edge: cap;
+  font-family: GmarketSansMedium;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const LogoImage = styled.img`
+  border-radius: 10px;
+  width: 6.875rem;
+  height: 6.64394rem;
+  cursor: pointer;
+`;
+
+export const GroupLargeCategory = styled.span`
+  color: #000;
+  leading-trim: both;
+  text-edge: cap;
+  font-family: GmarketSansLight;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  margin-left: 0.5rem;
+`;
+
+export const CategoriesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 0.3rem;
+`;
+
+export const GroupCategories = styled.span`
+  width: auto;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #dddddd;
+  border-radius: 3rem;
+  margin-right: 0.4rem;
+  padding: 0.3rem;
+  font-family: GmarketSansLight;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+`;
+
+export const GroupIntroduce = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  color: rgba(0, 0, 0, 0.8);
+  font-family: GmarketSansLight;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  margin-top: 0.4rem;
+`;
+
+export const FormContainer = styled.form`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const TitleInput = styled.input`
+  width: 100%;
+  height: 1.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: GmarketSansLight;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-top: 0.7rem;
+  padding-bottom: 0.7rem;
+  border: none;
+  border-bottom: 0.5px solid #dddddd;
+`;
+
+export const ContentInput = styled.textarea`
+  width: 34.8125rem;
+  height: 5.75rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  border: 1px solid #dbdbdf;
+  background: #fafafa;
+  font-family: GmarketSansLight;
+  font-size: 0.875rem;
+  font-style: normal;
+  line-height: normal;
+  padding: 0.5rem;
+  resize: none;
+`;
+
+export const FieldContainer = styled.div`
+  width: 34.8125rem;
+  height: 2.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const ApplicationField = styled.span`
+  width: 6rem;
+  height: 2.0625rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.625rem;
+  border: 1px solid #dbdbdf;
+  margin-right: 1rem;
+  font-family: GmarketSansLight;
+  font-size: 0.875rem;
+  font-style: normal;
+  line-height: normal;
+`;
+
+export const FieldInput = styled(TitleInput)`
+  margin-top: 1rem;
+`;
+
+export const ActiveContentInput = styled(ContentInput)``;
+
+export const ActivePeriodContainer = styled(FieldContainer)``;
+
+export const ActivePeriodBtn = styled.button`
+  width: 4.8125rem;
+  height: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.625rem;
+  border: 1px solid #dbdbdf;
+  margin-right: 1rem;
+  font-family: GmarketSansLight;
+  font-size: 0.875rem;
+  font-style: normal;
+  line-height: normal;
+  background-color: white;
+  cursor: pointer;
+`;
--- src/screen/Recruitment/CreateRecruitmentFirst.tsx
+++ src/screen/Recruitment/CreateRecruitmentFirst.tsx
@@ -5,16 +5,42 @@ import level_one from '../../assets/level-one.svg';
 import level_two from '../../assets/level-two.svg';
 import arrow from '../../assets/arrow.svg';
 import { Link } from 'react-router-dom';
+import img from '../../assets/img.svg';
 
-import { RecruitmentContainer } from '../../component/CreateRecruitment/CreateRecruitmentStyles';
+import {
+  RecruitmentContainer,
+  LogoImage,
+  InfoContainer,
+  TextContainer,
+  GroupName,
+  BasicNoticeTitle,
+  BasicNoticeText,
+  RedAsterisk,
+  GroupLargeCategory,
+  GroupCategories,
+  CategoriesContainer,
+  GroupContainer,
+  GroupIntroduce,
+  FormContainer,
+  TitleInput,
+  ContentInput,
+  FieldContainer,
+  ApplicationField,
+  FieldInput,
+  ActiveContentInput,
+  ActivePeriodContainer,
+  ActivePeriodBtn
+} from '../../component/CreateRecruitment/CreateRecruitmentStyles';
 
 const CreateRecruitmentFirst: React.FC = () => {
   return (
     <BackGround>
       <Introduction>
         <NoticeTitle>모집 공고 등록하기</NoticeTitle>
-        <SubtitleBlue>모집 공고 등록 가이드</SubtitleBlue>
-        <SubTitle>를 참고해 단체를 등록해보세요</SubTitle>
+        <SubtitleContainer>
+          <SubtitleBlue>모집 공고 등록 가이드</SubtitleBlue>
+          <SubTitle>를 참고해 단체를 등록해보세요</SubTitle>
+        </SubtitleContainer>
         <NoticeText>
           <img src={check_box} />
           <p>명지대학교에 등록된 모든 동아리 · 학회 · 학생 단체의 모집 공고</p>
@@ -29,14 +55,111 @@ const CreateRecruitmentFirst: React.FC = () => {
       </Introduction>
       <Board>
         <RecruitmentContainer>
-          <div>
-            <span>단체 기본 정보</span>
-          </div>
-          <div>
-            <span>모집 공고 등록 가이드</span>
-            <span>를 참고해 단체를 등록해보세요.</span>
-          </div>
+          <TextContainer>
+            <BasicNoticeTitle>단체 기본 정보</BasicNoticeTitle>
+          </TextContainer>
+          <TextContainer>
+            <BasicNoticeText>
+              모집 공고 등록 시 표시되는 단체 정보입니다.
+            </BasicNoticeText>
+          </TextContainer>
+          <GroupContainer>
+            <LogoImage src={img} alt='로고 이미지' />
+            <InfoContainer>
+              <TextContainer>
+                <GroupName>과일 러버</GroupName>
+                <GroupLargeCategory>소모임</GroupLargeCategory>
+                <CategoriesContainer>
+                  <GroupCategories>디자인/사진/영상</GroupCategories>
+                  <GroupCategories>체육/헬스</GroupCategories>
+                  <GroupCategories>IT/공학</GroupCategories>
+                </CategoriesContainer>
+                <GroupIntroduce>
+                  안녕하세요, 저희는 과일 러버 소모임 과일 러버단입니다. 저희의
+                  과일 사랑은 무한합니다. 사실 저는 수박만 좋아합니다. 다른 거를
+                  좋아하기 위한 소모임입니다.
+                </GroupIntroduce>
+              </TextContainer>
+            </InfoContainer>
+          </GroupContainer>
         </RecruitmentContainer>
+        {/* Form 시작 */}
+        <FormContainer>
+          <RecruitmentContainer>
+            {/* 제목 */}
+            <TextContainer>
+              <BasicNoticeTitle>모집 공고 내용 작성하기</BasicNoticeTitle>
+            </TextContainer>
+            <TextContainer>
+              <BasicNoticeText>모집 공고 제목</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+            <TitleInput
+              name='recruitmentTitle'
+              type='text'
+              placeholder='모집 공고 제목을 작성해주세요.'
+            />
+          </RecruitmentContainer>
+          <RecruitmentContainer>
+            {/* 설명글 */}
+            <TextContainer>
+              <BasicNoticeText>모집 설명글</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+            <ContentInput
+              name='description'
+              placeholder='모집 설명글을 작성해주세요. (최대 300자)'
+              maxLength={300}
+            />
+          </RecruitmentContainer>
+          <RecruitmentContainer>
+            {/* 모집 분야 */}
+            <TextContainer>
+              <BasicNoticeText>모집 분야 (최대 10개)</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+            <FieldContainer>
+              <ApplicationField>미디어팀</ApplicationField>
+              <ApplicationField>컨텐츠팀</ApplicationField>
+            </FieldContainer>
+            <FieldInput
+              name='applicationField'
+              type='text'
+              placeholder='모집 분야를 작성해주세요.'
+            />
+          </RecruitmentContainer>
+          <RecruitmentContainer>
+            {/* 활동 내용 */}
+            <TextContainer>
+              <BasicNoticeText>활동 내용</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+            <ActiveContentInput
+              name='activeContent'
+              placeholder='주요 활동 내용을 작성해주세요. (최대 500자)'
+              maxLength={500}
+            />
+          </RecruitmentContainer>
+          <RecruitmentContainer>
+            {/* 활동 기간 */}
+            <TextContainer>
+              <BasicNoticeText>활동 기간</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+            <ActivePeriodContainer>
+              <ActivePeriodBtn>한 학기</ActivePeriodBtn>
+              <ActivePeriodBtn>1년</ActivePeriodBtn>
+              <ActivePeriodBtn>1년 이상</ActivePeriodBtn>
+            </ActivePeriodContainer>
+          </RecruitmentContainer>
+          <RecruitmentContainer>
+            {/* 모집 기간 */}
+            <TextContainer>
+              <BasicNoticeText>모집 기간</BasicNoticeText>
+              <RedAsterisk>*</RedAsterisk>
+            </TextContainer>
+          </RecruitmentContainer>
+        </FormContainer>
       </Board>
       <Level>
         <img src={level_one} />
@@ -65,7 +188,7 @@ const Board = styled.div`
   background: #fff;
   padding: 20px;
   width: 659px;
-  height: 770px;
+  height: auto;
   box-shadow: 0px 4px 20px 3px rgba(0, 0, 0, 0.05);
   margin-bottom: 21px;
   margin-left: 8px;
@@ -131,6 +254,8 @@ const SaveButton = styled.button`
   &:active {
     color: #358e48;
   }
+
+  cursor: pointer;
 `;
 
 const NextButton = styled.button`
@@ -168,6 +293,9 @@ const NextButton = styled.button`
     text-align: center;
     margin-left: 12px;
   }
+
+  cursor: pointer;
+
 `;
 
 const NoticeTitle = styled.h1`
@@ -182,6 +310,13 @@ const NoticeTitle = styled.h1`
   margin-bottom: 13px;
 `;
 
+const SubtitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
 const SubTitle = styled.p`
   display: flex;
   align-items: left;
@@ -194,6 +329,7 @@ const SubTitle = styled.p`
 `;
 
 const SubtitleBlue = styled(SubTitle)`
+  font-family: GmarketSansMedium;
   color: #32a9eb;
 `;
 
