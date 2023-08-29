817b8ed5953b38e8f8012c4e6cd1ba1d83bbe0e2
동우 <kanggo00@gmail.com>
2023-08-30 07:38:01 +0900

    fix 0737

--- src/component/Header/PostModalContent.tsx
+++ src/component/Header/PostModalContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as Styled from './HeaderStyles';
 import recruitmentImg from '../../assets/Header/recruitmentImg1.svg';
 import arrowRight from '../../assets/Header/arrow-right1.svg';
-import organizationImg from '../../assets/Header/organizationImg.svg';
+import organizationImg from '../../assets/Header/organizationImg1.svg';
 import { Link } from 'react-router-dom';
 
 const PostModalContent = ({
