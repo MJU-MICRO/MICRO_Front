1a9bd300e827138624160b5c37769757ebdd32df
동우 <kanggo00@gmail.com>
2023-08-30 07:34:18 +0900

    fix 0830

--- src/component/Header/PostModalContent.tsx
+++ src/component/Header/PostModalContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Styled from './HeaderStyles';
-import recruitmentImg from '../../assets/Header/recruitmentImg.svg';
+import recruitmentImg from '../../assets/Header/recruitmentImg1.svg';
 import arrowRight from '../../assets/Header/arrow-right.svg';
 import organizationImg from '../../assets/Header/organizationImg.svg';
 import { Link } from 'react-router-dom';
